d8e354e04b23a9edfec62a262887f9b92b898b62
Chris Lin <51550194+Chrislin923@users.noreply.github.com>
2021-06-29 11:45:52 +0800

    Add files via upload

---
+++ csc-335-minesweeper-csc335-jesse-derek-sarthak-chris/src/MinesweeperMain.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import view.MinesweeperView;
+
+/**
+ * The main for minesweeper
+ * @author Chris Lin, Sarthak Bawal, Derek Tominaga, Jesse Gomez
+ *
+ */
+public class MinesweeperMain {
+
+	public static void main(String[] args) {
+		Application.launch(MinesweeperView.class, args);
+	}
+
+}
---
+++ csc-335-minesweeper-csc335-jesse-derek-sarthak-chris/src/main/Minesweeper.java
@@ -0,0 +1,18 @@
+
+package main;
+
+import javafx.application.Application;
+import view.MinesweeperView;
+
+/**
+ * Main for the minesweeper
+ * @author Chris Lin, Sarthak Bawal, Derek Tominaga, Jesse Gomez
+ *
+ */
+public class Minesweeper {
+	
+	public static void main(String[] args) {
+		Application.launch(MinesweeperView.class, args);
+	}
+
+}
